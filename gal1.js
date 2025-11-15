9ee2264a480150cbbcb3c7962d939c4ec5b483e9
yeni12134321 <hernandez.yenifet.23.26@gmail.com>
2025-11-14 21:41:12 -0700

    Add files via upload

---
+++ gal1.js
@@ -0,0 +1,36 @@
+function openModal() {
+    document.getElementById("migaleria").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("migaleria").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("misImagenes");
+    var dots = document.getElementsByClassName("miniatura");
+    var describeText = document.getElementById("describe");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    describeText.innerHTML = dots[slideIndex - 1].alt;
+}
